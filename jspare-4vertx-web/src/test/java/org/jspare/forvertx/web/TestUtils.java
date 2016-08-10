fede67182ba5c65a234c83b1b942f135350661e1
paulo.lima <paulo.lima@knowtech.net.br>
2016-08-10 19:02:48 -0300

    test to be fixed

---
+++ jspare-4vertx-web/src/test/java/org/jspare/forvertx/web/TestUtils.java
@@ -0,0 +1,13 @@
+package org.jspare.forvertx.web;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestUtils {
+	
+	public static <T> Set<T> emptySet(){
+		
+		return new HashSet<T>();
+	}
+
+}
--- jspare-4vertx-web/src/test/java/org/jspare/forvertx/web/handler/HandlerCollectorTest.java
+++ jspare-4vertx-web/src/test/java/org/jspare/forvertx/web/handler/HandlerCollectorTest.java
@@ -1,16 +1,34 @@
 package org.jspare.forvertx.web.handler;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jspare.forvertx.web.TestUtils;
+import org.jspare.forvertx.web.handler.mock.routes.MultiRoutes;
+import org.jspare.forvertx.web.middleware.Middleware;
+import org.junit.Assert;
 import org.junit.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class HandlerCollectorTest {
 	
 	@Test
 	public void collectRouteSetTest(){
 		
+		Set<HandlerData> handlers = HandlerCollector.collect(MultiRoutes.class, TestUtils.emptySet(), TestUtils.emptySet());
+		Assert.assertEquals(2, handlers.size());
 	}
 	
 	@Test
 	public void collectMockBoostrapTest(){
 		
 	}
+	
+	private Set<Middleware> emptyMiddlewares(){
+		
+		return new HashSet<>();
+	}
+	
 }
